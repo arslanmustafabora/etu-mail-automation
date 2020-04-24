eae6a538a5d46d83a3fd629c9df1b328cc082546
arslanmustafabora <mboraarslan1999@gmail.com>
2020-04-24 16:40:23 +0300

    Face detection added
    Firstly I applied only opencv, it will be improved

--- TobbMailAutomation.py
+++ TobbMailAutomation.py
@@ -1,27 +1,30 @@
 from selenium import webdriver
+import faceDetect
 
 URL = "https://webmail.etu.edu.tr/roundcube/?_task=mail&_mbox=INBOX"
 
 ID = input("Enter your ID:")
 PASS = input("Enter your password: ")
+found = faceDetect.detect()
 
-br = webdriver.Chrome()
-br.get(URL)
+if found == 1:
+    br = webdriver.Chrome()
+    br.get(URL)
 
-userID = br.find_element_by_xpath("//*[@id='rcmloginuser']")
-password = br.find_element_by_xpath("//*[@id='rcmloginpwd']")
-button = br.find_element_by_xpath("//*[@id='rcmloginsubmit']")
-
-
-userID.send_keys(ID)
-password.send_keys(PASS)
-button.click()
-br.maximize_window()
+    userID = br.find_element_by_xpath("//*[@id='rcmloginuser']")
+    password = br.find_element_by_xpath("//*[@id='rcmloginpwd']")
+    button = br.find_element_by_xpath("//*[@id='rcmloginsubmit']")
 
 
-subjects = br.find_elements_by_class_name("message")
+    userID.send_keys(ID)
+    password.send_keys(PASS)
+    button.click()
+    br.maximize_window()
 
-for subject in subjects:
-    print(subject.text)
 
-br.quit()
+    subjects = br.find_elements_by_class_name("message")
+
+    for subject in subjects:
+        print(subject.text)
+
+    br.quit()
