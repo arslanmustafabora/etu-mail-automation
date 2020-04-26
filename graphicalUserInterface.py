aa2e71759b85b127d69208f29c83d7f0ee8efc69
arslanmustafabora <mboraarslan1999@gmail.com>
2020-04-26 18:11:30 +0300

    Login screen added

--- graphicalUserInterface.py
+++ graphicalUserInterface.py
@@ -1,5 +1,12 @@
 import pyautogui
 
+def login():
+
+	userName = pyautogui.prompt(text = "Enter your username", title = "USERNAME", default = "")
+	password = pyautogui.password(text = "Enter your password", title = "PASSWORD", default = "")
+	return userName, password
+
+
 def screen(subjects):
 
 	empty = ""
