1941c85c3a0abe2f27064721ebac214d3edbdd70
arslanmustafabora <mboraarslan1999@gmail.com>
2020-04-26 19:24:16 +0300

    Some changes

--- TobbMailAutomation.py
+++ TobbMailAutomation.py
@@ -3,35 +3,23 @@ from selenium.webdriver.chrome.options import Options
 import faceDetect
 import graphicalUserInterface
 
-
 URL = "https://webmail.etu.edu.tr/roundcube/?_task=mail&_mbox=INBOX"
-
-ID,PASS = graphicalUserInterface.login()
-
+ID, PASS = graphicalUserInterface.login()
 found = faceDetect.detect()
 
 if found == 1:
 
     opt = Options()
     opt.add_argument("headless")
-
     br = webdriver.Chrome(options=opt)
     br.get(URL)
-
     userID = br.find_element_by_xpath("//*[@id='rcmloginuser']")
     password = br.find_element_by_xpath("//*[@id='rcmloginpwd']")
     button = br.find_element_by_xpath("//*[@id='rcmloginsubmit']")
-
-
     userID.send_keys(ID)
     password.send_keys(PASS)
     button.click()
     br.maximize_window()
-
     subjects = br.find_elements_by_class_name("message")
-
     graphicalUserInterface.screen(subjects)
     br.quit()
-
-
-
--- graphicalUserInterface.py
+++ graphicalUserInterface.py
@@ -1,18 +1,10 @@
 import pyautogui
-
 def login():
-
 	userName = pyautogui.prompt(text = "Enter your username", title = "USERNAME", default = "")
 	password = pyautogui.password(text = "Enter your password", title = "PASSWORD", default = "")
 	return userName, password
-
-
 def screen(subjects):
-
 	empty = ""
 	for i in range(20):
-
 		empty += str(subjects[i].text)+"\n"
-
 	pyautogui.alert(text= empty, title = "Recently", button = "Okey")
-   	
